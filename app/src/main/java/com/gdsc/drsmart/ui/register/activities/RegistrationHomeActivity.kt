88e520b21e6ff355da0ea97107e89cad454abdaf
Amr Hossam <amrhossam.dev@gmail.com>
2022-02-16 15:58:39 +0200

    Initial commit

---
+++ app/src/main/java/com/gdsc/drsmart/ui/register/activities/RegistrationHomeActivity.kt
@@ -0,0 +1,15 @@
+package com.gdsc.drsmart.ui.register.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.gdsc.drsmart.R
+import kotlinx.android.synthetic.main.activity_registration_home.*
+
+class RegistrationHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_home)
+
+    }
+}
